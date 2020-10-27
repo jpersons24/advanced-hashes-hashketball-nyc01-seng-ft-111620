9ba3e9a05be411625cca8ccdecf6c24ecfd2c107
Jakob Persons <jakobep24@gmail.com>
2020-10-27 17:33:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -211,22 +211,22 @@ def get_players_array
     team_details[:players]
   end
 end
-binding.pry
+# binding.pry
 
 
-def big_shoe_rebounds
-  big_shoes_guy = 0
-  rebounds = 0
-    game_hash.each do |team, team_details|
-      team_details[:players].each do |stats|
-        if stats[:shoe] > big_shoes_guy
-          big_shoes_guy = stats[:shoe]
-          rebounds = stats[:rebounds]
-        end
-      end
-    end
-    rebounds
-end
+# def big_shoe_rebounds
+#   big_shoes_guy = 0
+#   rebounds = 0
+#     game_hash.each do |team, team_details|
+#       team_details[:players].each do |stats|
+#         if stats[:shoe] > big_shoes_guy
+#           big_shoes_guy = stats[:shoe]
+#           rebounds = stats[:rebounds]
+#         end
+#       end
+#     end
+#     rebounds
+# end
 # binding.pry
 
 def big_shoe_rebounds
